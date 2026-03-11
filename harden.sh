5bc99e4b01b933f6571422d43e37094e122f8550
CK-Bachoo <CKizzzle@gmail.com>
2026-03-11 18:22:21 -0400

    SUBMISSION: Session 03 - Malicious IP Report and hardening fix

--- harden.sh
+++ harden.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-chmod 600 ./Vault_Mock/secrets.txt
-chmod 640 ./Vault_Mock/shadow_mock
+[[ $EUID -ne 0 ]] && echo "Error: Run as root" && exit 1
+sed -i 's/^#PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
+sed -i 's/^#PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config
+systemctl restart sshd
+ufw default deny incoming
+ufw allow ssh
+ufw allow 80/tcp
+ufw allow 443/tcp
+ufw --force enable
 echo "System Hardening Applied."
