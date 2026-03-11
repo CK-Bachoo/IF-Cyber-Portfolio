8be6c78853be982fa6f79ac52cf92bd9a65015b2
CK-Bachoo <ck-bachoo@users.noreply.github.com>
2026-03-11 15:30:34 -0400

    AUDIT: Local mock hardening applied

--- harden.sh
+++ harden.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-[[ $EUID -ne 0 ]] && echo "Error: Run as root" && exit 1
-sed -i "s/^#PermitRootLogin.*/PermitRootLogin no/" /etc/ssh/sshd_config && sed -i "s/^#PasswordAuthentication.*/PasswordAuthentication no/" /etc/ssh/sshd_config && systemctl restart sshd
-ufw default deny incoming && ufw allow ssh && ufw allow 80/tcp && ufw allow 443/tcp && ufw --force enable
-chmod 600 ~/Vault/secrets.txt && chmod 640 /etc/shadow
+chmod 600 ./Vault_Mock/secrets.txt
+chmod 640 ./Vault_Mock/shadow_mock
 echo "System Hardening Applied."
