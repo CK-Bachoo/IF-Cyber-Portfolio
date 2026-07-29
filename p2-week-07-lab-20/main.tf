7021526f64211ac9ab84799e0eb2243947da036d
C.K. Bachoo <ckizzzle@gmail.com>
2026-07-29 00:28:45 +0000

    p2-week-07-lab-20: Globally enforced explicit public access blocks and customer KMS encryption keys

--- p2-week-07-lab-20/main.tf
+++ p2-week-07-lab-20/main.tf
@@ -2,11 +2,37 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# tfsec:ignore:aws-s3-encryption-customer-key
-# tfsec:ignore:aws-s3-no-public-access-block
-# tfsec:ignore:aws-s3-enable-versioning
+resource "aws_kms_key" "s3_vault_key" {
+  description             = "KMS key for hardened S3 data vault"
+  deletion_window_in_days = 7
+}
+
 resource "aws_s3_bucket" "secured_bucket" {
   bucket_prefix = "titan-hardened-data-vault-"
-  acl           = "private"
   force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.secured_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.secured_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.s3_vault_key.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_block" {
+  bucket                  = aws_s3_bucket.secured_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
