6223b2994db23e33cf4d8d6dd2872729b7fdb06c
C.K. Bachoo <ckizzzle@gmail.com>
2026-08-06 00:30:10 +0000

    feat(p2-week-08-lab-23): Codified enterprise least-privilege execution policy rulesets

---
+++ p2-week-08-lab-23/lambda_function.py
@@ -0,0 +1,8 @@
+import json
+
+def lambda_handler(event, context):
+    print("The Ghost Fleet is operational.")
+    return {
+        "statusCode": 200,
+        "body": json.dumps("DevSecOps Execution Complete!")
+    }
